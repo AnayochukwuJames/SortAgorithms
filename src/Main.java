e296b90ec011b0c6e7db7f7d6d91138c57a103c6
Anayochukwu James <anayochukwujames11@gmail.com>
2024-09-22 16:28:08 +0100

    Update initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int arr[] = {5,4,7,2,8,1,3};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Original Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
         }
+
+        int temp =0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i +1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.print("\nSorted Array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i]);
+        }
+
     }
 }
